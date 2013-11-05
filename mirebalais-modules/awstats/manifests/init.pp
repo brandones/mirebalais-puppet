27064109be2e22ff74276ac9f05f6c44c4a4e6ae
mogoodrich <mgoodrich@pih.org>
2013-11-05 16:54:55 -0500

    fixed formatting

--- mirebalais-modules/awstats/manifests/init.pp
+++ mirebalais-modules/awstats/manifests/init.pp
@@ -1,23 +1,19 @@
 class awstats(
       $site_domain = hiera('site_domain')
-
 ) {
 
-      package { 'awstats':
-      	      ensure => installed,
-	      }
+    package { 'awstats':
+  	    ensure => installed,
+    }
 
-file { "/etc/awstats/awstats.$site_domain.conf":
+    file { "/etc/awstats/awstats.$site_domain.conf":
+        ensure => present,
+        content => template('awstats/awstats.conf.erb')
+    }
 
-     ensure => present,
-     content => template('awstats/awstats.conf.erb')
-}
-
-exec { 'generate initial awstats':
-
-     command => "/usr/lib/cgi-bin/awstats.pl -config=$site_domain -update",
-     subscribe => [ Package['awstats'], File["/etc/awstats/awstats.$site_domain.conf"]]
- 
-}
+    exec { 'generate initial awstats':
+        command => "/usr/lib/cgi-bin/awstats.pl -config=$site_domain -update",
+        subscribe => [ Package['awstats'], File["/etc/awstats/awstats.$site_domain.conf"]]
+    }
 
 }
