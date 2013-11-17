24a4408d2bdf258bed553d002b77cc430ae975c1
root <root@concepts.pih-emr.org>
2013-11-17 19:18:42 +0000

    updated awstats configuration

--- mirebalais-modules/awstats/manifests/init.pp
+++ mirebalais-modules/awstats/manifests/init.pp
@@ -7,8 +7,19 @@ class awstats(
     }
 
     file { "/etc/awstats/awstats.$site_domain.conf":
-        ensure => present,
-        content => template('awstats/awstats.conf.erb')
+    	 ensure => present,
+	 content => template('awstats/awstats.conf.erb')
+    }
+
+    file { '/usr/lib/cgi-bin/.htpasswd':
+    	 ensure => present,
+	 source => 'puppet:///modules/awstats/htpasswd'
+    }
+
+    cron { 'update awstats':
+    	 command => "/usr/lib/cgi-bin/awstats.pl -config=$site_domain -update > /dev/null",
+	 user => root,
+	 hour => 0
     }
 
     exec { 'generate initial awstats':
