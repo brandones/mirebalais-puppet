ed26186c6ff9318ef9dd1cab238fb91aa09ab5fb
Mário Areias <mario.s.s.areias@gmail.com>
2014-01-10 18:15:43 -0200

    UHM-956 Trying mysql_upgrade with root password

--- mirebalais-modules/mysql_upgrade/manifests/init.pp
+++ mirebalais-modules/mysql_upgrade/manifests/init.pp
@@ -55,7 +55,7 @@ class mysql_upgrade ($root_password = decrypt(hiera('mysql_root_password'))) {
   }
 
   exec { 'update_db':
-    command => 'mysql_upgrade',
+    command => 'mysql_upgrade -u root -p ${root_password}',
     require => [ Service['mysqlserver'] ],
   }
 
