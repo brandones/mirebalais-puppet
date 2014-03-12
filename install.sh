5c4471c559a242d1f955a0f8f45e8469f37139ef
mogoodrich <mgoodrich@pih.org>
2014-03-12 11:32:02 -0400

    added comment

--- install.sh
+++ install.sh
@@ -45,6 +45,8 @@ fi
 
 
 apt-get update
+
+# hack to make sure we have ruby1.9 installed instead of ruby1.8
 apt-get remove ruby1.8
 apt-get install -y ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 \
 build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev
