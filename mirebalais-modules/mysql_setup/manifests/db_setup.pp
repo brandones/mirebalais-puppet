8d3794ef9189484d2e87de7b8b30443c759c5313
Mário Areias <mario.s.s.areias@gmail.com>
2014-03-26 18:08:15 -0400

    UHM-956 using new api for manage mysql

--- mirebalais-modules/mysql_setup/manifests/db_setup.pp
+++ mirebalais-modules/mysql_setup/manifests/db_setup.pp
@@ -7,53 +7,68 @@ class mysql_setup::db_setup(
   $mirth_db_password = decrypt(hiera('mirth_db_password')),
 ) {
 
-  database { $openmrs_db :
+  mysql_database { $openmrs_db :
     ensure  => present,
-    require => [Service['mysqld'], Package['mirebalais']],
+    require => Service['mysqld'],  Package['mirebalais']],
     charset => 'utf8',
   } ->
 
-  database_user { "${openmrs_db_user}@localhost":
+  mysql_user { "${openmrs_db_user}@localhost":
     ensure        => present,
     password_hash => mysql_password($openmrs_db_password),
-    require       => Service['mysqld'],
+    require => Service['mysqld'], Package['mirebalais']],
   } ->
 
-  database_grant { "${openmrs_db_user}@localhost/${openmrs_db}":
-    privileges => ['all'],
-    require    => Service['mysqld'],
+  mysql_grant { "${openmrs_db_user}@localhost/${openmrs_db}":
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table => '*.*',
+    user => "${openmrs_db_user}@localhost",
+    require => Service['mysqld'],  Package['mirebalais']],
   } ->
 
-  database_grant { "root@localhost/${openmrs_db}":
-    privileges => ['all'],
-    require    => Service['mysqld'],
+  mysql_grant { "root@localhost/${openmrs_db}":
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table => '*.*',
+    user => "root@localhost",
+    require => Service['mysqld'],  Package['mirebalais']],
     notify     => Openmrs::Liquibase_migrate ['migrate base schema'];
   }
 
-  database { $mirth_db :
+  mysql_database { $mirth_db :
     ensure  => present,
     charset => 'utf8',
     require => [Service['mysqld'], Package['mirebalais']],
   }
 
-  database_user { "${mirth_db_user}@localhost":
+  mysql_user { "${mirth_db_user}@localhost":
     ensure        => present,
     password_hash => mysql_password($mirth_db_password),
     require       => Service['mysqld'],
   } ->
 
-  database_grant { "${mirth_db_user}@localhost/${mirth_db}":
-    privileges => ['all'],
+  mysql_grant { "${mirth_db_user}@localhost/${mirth_db}":
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table => '*.*',
+    user => "${mirth_db_user}@localhost",
     require    => Service['mysqld'],
   } ->
 
-  database_grant { "root@localhost/${mirth_db}":
-    privileges => ['all'],
+  mysql_grant { "root@localhost/${mirth_db}":
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table => '*.*',
+    user => "root@localhost",
     require    => Service['mysqld'],
   }
 
-  database_grant { "${mirth_db_user}@localhost/${openmrs_db}.pacsintegration_outbound_queue":
-    privileges => ['all'],
-    require    => [ Service['mysqld'], Database[$openmrs_db] ]
+  mysql_grant { "${mirth_db_user}@localhost/${openmrs_db}.pacsintegration_outbound_queue":
+    options    => ['GRANT'],
+    privileges => ['ALL'],
+    table => '*.*',
+    user => "{mirth_db_user}@localhost",
+    require    => [ Service['mysqld'], Mysql_database[$openmrs_db] ]
   }
 }
