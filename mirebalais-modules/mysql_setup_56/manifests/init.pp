dde68ccb350ca6f8479b3caaa78e192db507c5a6
mogoodrich <mgoodrich@pih.org>
2014-05-01 13:53:12 -0400

    UHM-1228: Fix mysql_setup_56 puppet module so that it doesn't re-install mysql 5.5 on every run

--- mirebalais-modules/mysql_setup_56/manifests/init.pp
+++ mirebalais-modules/mysql_setup_56/manifests/init.pp
@@ -2,15 +2,6 @@ class mysql_setup_56 (
   $root_password = decrypt(hiera('mysql_root_password'))
 ) {
 
-  class { 'mysql::server':
-    manage_service => false,
-    config_hash    => {
-      'root_password' => $root_password,
-      'config_file'   => '/tmp/my.cnf',
-      'restart'       => false
-    },
-  } ->
-
   apt::source { 'mysql':
     ensure      => present,
     location    => 'http://bamboo.pih-emr.org/mysql-repo',
@@ -29,71 +20,90 @@ class mysql_setup_56 (
     require => [ Package['libaio1'] ],
   }
 
+  user { 'mysql':
+    ensure => 'present',
+    home   => "/home/mysql",
+    shell  => '/bin/sh',
+  }
+
   file { '/opt/mysql/server-5.6/':
     ensure  => present,
     owner   => mysql,
     group   => mysql,
     recurse => inf,
-    require => [ Package['mysql'] ],
+    require => [ Package['mysql'], User['mysql'] ],
+  }
+
+  file { "/etc/mysql":
+    ensure  => directory,
+    owner   => mysql,
+    group   => mysql,
+    mode    => '0755',
+    require => User['mysql'],
+  }
+
+  file { "/etc/mysql/conf.d":
+    ensure  => directory,
+    owner   => mysql,
+    group   => mysql,
+    mode    => '0755',
+    require => File['/etc/mysql'],
   }
 
   file { '/etc/mysql/my.cnf':
     ensure  => file,
-    content => template('mysql_upgrade/my.cnf.erb'),
-    require => [ File['/opt/mysql/server-5.6/'] ],
-  } ~>
-
-  package { 'mysql-common':
-    ensure  => absent,
-    require => [ File['/etc/mysql/my.cnf'] ],
+    content => template('mysql_setup_56/my.cnf.erb'),
+    require => [ File['/etc/mysql'] ],
   }
 
-  package { 'mysql-server-5.5':
-    ensure  => absent,
-    require => [ Package['mysql-common'] ],
+  file { '/usr/bin/mysqladmin':
+    ensure => link,
+    target => '/opt/mysql/server-5.6/bin/mysqladmin',
+    require => Package['mysql'],
   }
 
-  package { 'mysql-server-core-5.5':
-    ensure  => absent,
-    require => [ Package['mysql-server-5.5'] ],
+  exec { 'set_mysql_rootpassword':
+    command     => "mysqladmin -u root password '${root_password}'",
+    logoutput   => true,
+    environment => "HOME=${root_home}",
+    unless      => "mysqladmin -u root -p'${root_password}' status > /dev/null",
+    path        => '/usr/local/sbin:/usr/bin:/usr/local/bin',
+    require     => Service['mysqld'],
   }
 
-  package { 'mysql-client-5.5':
-   ensure  => absent,
-   require => [ Package['mysql-server-core-5.5'] ],
-  }
-
-  package { 'mysql-client-core-5.5':
-    ensure  => absent,
-    require => [ Package['mysql-client-5.5'] ],
+  file { "root_user_my.cnf":
+    path        => "${root_home}/.my.cnf",
+    content     => template('mysql_setup_56/my.cnf.pass.erb'),
+    require     => Exec['set_mysql_rootpassword'],
   }
 
   exec { 'install_db':
-    command => "mysql_install_db --user=mysql --datadir=/var/lib/mysql",
-    path => ["/opt/mysql/server-5.6/scripts", "/opt/mysql/server-5.6/bin"],
-    creates => "/etc/init.d/mysql.server",
-    require => [ Package['mysql-client-core-5.5'] ],   
+    creates   => '/etc/init.d/mysql.server',  # this just means that this not execute if this mysql.server file has been created (i.e., prevents this from being run twice)
+    command   => "mysql_install_db --user=mysql --datadir=/var/lib/mysql",
+    path      => ["/opt/mysql/server-5.6/scripts", "/opt/mysql/server-5.6/bin"],
+    require   => [ Package['mysql'], User['mysql'] ],
   }
 
   file { '/usr/bin/mysql':
       ensure => link,
       target => '/opt/mysql/server-5.6/bin/mysql',
-      require => [ Exec['install_db'] ],
+      require => [ Package['mysql'] ],
   }
  
   file { '/etc/init.d/mysql.server':
-    source  => '/opt/mysql/server-5.6/support-files/mysql.server',
     ensure  => present,
-    recurse => inf,
-    require => [ File['/usr/bin/mysql'] ],
+    source  => '/opt/mysql/server-5.6/support-files/mysql.server',
+    mode    => '0755',
+    owner   => 'mysql',
+    group   => 'mysql',
+    require => [ Exec['install_db'] ],
   }
 
   service { 'mysqld':
     ensure  => running,
     name    => 'mysql.server',
     enable  => true,
-    require => [ File['/etc/init.d/mysql.server'] ],
-    
+    require => [ Exec['install_db'], File['/etc/init.d/mysql.server/'], File['/opt/mysql/server-5.6/'], File['/etc/mysql/conf.d'], File['/etc/mysql/my.cnf']  ],
   }
 
 }
