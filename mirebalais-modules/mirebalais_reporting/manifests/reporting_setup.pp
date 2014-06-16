de7e9b47655c8a6d7276b998bf4b6514f1cbe5e6
Mark Goodrich <mgoodrich@pih.org>
2014-06-16 11:04:20 -0400

    Tweak to comments

--- mirebalais-modules/mirebalais_reporting/manifests/reporting_setup.pp
+++ mirebalais-modules/mirebalais_reporting/manifests/reporting_setup.pp
@@ -4,7 +4,7 @@ class mirebalais_reporting::reporting_setup (
   	$openmrs_db_password = decrypt(hiera('openmrs_db_password'))
 ) {
 	
-	# note that public/private key sharing still needs to be set up manually between production and reporting
+	# note that public/private key sharing needs to be set up manually between production and reporting
 	user { backups:
     		ensure => 'present',
     		home   => "/home/backups",
