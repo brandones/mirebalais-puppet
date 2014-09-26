e768e2ff315f4c715b87a83d388c54d0cda80cc3
mogoodrich <mgoodrich@pih.org>
2014-09-26 11:28:25 -0400

    add automatic security upgrades via apt

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node default {
     always_apt_update => true,
   }
 
-  include apt-upgrades
+  include apt_upgrades
   include wget
   include java
   include mysql_setup_56
---
+++ mirebalais-modules/apt_upgrades/manifests/init.pp
@@ -0,0 +1,17 @@
+class apt_ugrades() 
+{
+
+	class { 'apt::unattended_upgrades' :
+  	      origins 	          => ['${distro_id} ${distro_codename}-security'],
+ 	      blacklist           => ['mirebalais'],
+  	      update              => '1',
+  	      download            => '1',
+  	      upgrade             => '1',
+  	      autoclean           => '7',
+  	      mail_to	          => 'mgoodrich@pih.org',
+  	      auto_fix            => true,
+  	      minimal_steps       => false,
+  	      install_on_shutdown => false,
+  	      auto_reboot         => false
+	 }
+}	      
