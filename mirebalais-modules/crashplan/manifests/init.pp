6c2136af894af4e0d3c1b1051a1bdb3de8d52a76
lnball <eball@pih.org>
2015-06-03 16:25:45 -0400

    Fixed the prefix for backup files;  Changed to CrashPlanPro (from CrashPlan)

--- mirebalais-modules/crashplan/manifests/init.pp
+++ mirebalais-modules/crashplan/manifests/init.pp
@@ -3,15 +3,15 @@ class crashplan(
 ){
 
   wget::fetch { 'download':
-    source      => 'http://download.crashplan.com/installs/linux/install/CrashPlan/CrashPlan_3.0.3_Linux.tgz',
-    destination => '/usr/local/crashplan.tgz',
+    source      => 'http://download.crashplan.com/installs/linux/install/CrashPlanPRO/CrashPlanPRO_4.2.0_Linux.tgz',
+    destination => '/usr/local/crashplanpro.tgz',
     timeout     => 0,
     verbose     => false,
   }
 
   exec { 'unzip':
     cwd     => '/usr/local',
-    command => '/bin/tar -xzf /usr/local/crashplan.tgz',
+    command => '/bin/tar -xzf /usr/local/crashplanpro.tgz',
     require => [ Wget::Fetch['download'] ],
   }
 }
