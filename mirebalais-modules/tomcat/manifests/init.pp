7b32aa4b75ed506fc6e869e62afc6caf5b9febe5
mogoodrich <mgoodrich@pih.org>
2015-06-30 15:49:45 -0400

    uninstall tomcat6 when installing tomcat7

--- mirebalais-modules/tomcat/manifests/init.pp
+++ mirebalais-modules/tomcat/manifests/init.pp
@@ -8,15 +8,43 @@ class tomcat (
 
   case $tomcat {
     tomcat6: {
-      $version = '6.0.36'
-      $source  = 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.36/bin/apache-tomcat-6.0.36.tar.gz'
+      	$old_tomcat = 'tomcat0'
+	$old_version = '0.0.0'
+	$version = '6.0.36'
+      	$source  = 'http://archive.apache.org/dist/tomcat/tomcat-6/v6.0.36/bin/apache-tomcat-6.0.36.tar.gz'
     }
     tomcat7: {
-      $version = '7.0.62'
-      $source  = 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz'
+	$old_tomcat = 'tomcat6'
+	$old_version = '6.0.36'
+      	$version = '7.0.62'
+      	$source  = 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz'
     }
   }
 
+  # uninstall old version of tomcat
+  file { "/usr/local/apache-tomcat-${old_version}":
+	ensure => absent,
+	force => true
+  }
+  
+  file { "/etc/init.d/${old_tomcat}":
+    ensure  => absent
+  }
+
+  file { "/etc/default/${old_tomcat}":
+    ensure  => absent
+  }
+
+  file { "/etc/logrotate.d/${old_tomcat}":
+    ensure  => absent
+  }
+
+  file { "/home/${old_tomcat}":
+    ensure  => absent,
+    force => true
+  }
+
+  # install the proper version of tomcat
   wget::fetch { 'download-tomcat':
     source      => $source,
     destination => "/usr/local/tomcat-${version}.tgz",
@@ -99,5 +127,17 @@ class tomcat (
   service { $tomcat:
     enable  => $services_enable,
     require => [ Exec['tomcat-unzip'], File["/usr/local/${tomcat}"], File["/etc/init.d/${tomcat}"] ],
-  }
+  }  
+
+  # ensure that the symbolic link for the old tomcat now points to the new tomcat (necessary until we 
+  # update the debian package to deploy to tomcat6
+
+#  file { "/usr/local/${old_tomcat}":
+#    ensure  => 'link',
+#    target  => "/usr/local/apache-tomcat-${version}",
+#    owner   => $tomcat,
+#    group   => $tomcat,
+#    require => Exec['tomcat-unzip'],
+#  }
+
 }
