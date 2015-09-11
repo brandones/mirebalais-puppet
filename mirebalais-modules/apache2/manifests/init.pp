a972dbeb74774a8e6134cc30b2e933350eeb923e
mseaton <mseaton@pih.org>
2015-09-11 10:51:11 -0400

    Make apache script run in order

--- mirebalais-modules/apache2/manifests/init.pp
+++ mirebalais-modules/apache2/manifests/init.pp
@@ -11,54 +11,54 @@ class apache2 (
   
   package { 'apache2':
     ensure => installed,
-  }
+  } ->
 
   package { 'libapache2-mod-jk':
     ensure => installed,
-  }
+  } ->
 
   file { '/etc/logrotate.d/apache2':
     ensure  => file,
     content  => template('apache2/logrotate.erb'),
-  }
+  } ->
 
   file { '/etc/apache2/workers.properties':
     ensure  => present,
     content => template('apache2/workers.properties.erb'),
     notify  => Service['apache2']
-  }
+  } ->
 
   file { '/etc/apache2/mods-available/jk.conf':
     ensure => present,
     source => 'puppet:///modules/apache2/jk.conf',
     notify => Service['apache2']
-  }
+  } ->
 
   file { '/etc/apache2/sites-available/default-ssl.conf':
     ensure => present,
     content => template('apache2/default-ssl.conf.erb'),
     notify => Service['apache2']
-  }
+  } ->
 
   file { '/etc/apache2/sites-available/default':
     ensure => file,
     source => 'puppet:///modules/apache2/sites-available/default',
     notify => Service['apache2']
-  }
+  } ->
 
   file { "/etc/ssl/certs/${ssl_cert_file}":
     ensure => file,
     source => "puppet:///modules/apache2/etc/ssl/certs/${ssl_cert_file}",
-  }
+  } ->
 
   file { "/etc/ssl/certs/${ssl_chain_file}":
     ensure => file,
     source => "puppet:///modules/apache2/etc/ssl/certs/${ssl_chain_file}",
-  }
+  } ->
 
   file { "/etc/ssl/private/${ssl_key_file}":
     ensure => present,
-  }
+  } ->
 
   exec { 'enable apache mods':
     command     => 'a2enmod jk && a2enmod deflate && a2enmod ssl && a2ensite default-ssl && a2enmod rewrite',
@@ -66,7 +66,7 @@ class apache2 (
     subscribe   => [ Package['apache2'], Package['libapache2-mod-jk'] ],
     refreshonly => true,
     notify      => Service['apache2']
-  }
+  } ->
 
   service { 'apache2':
     ensure   => $services_ensure,
