2ba3d16b974b027a8ea1d575ff4b83dd44623901
mseaton <mseaton@pih.org>
2015-09-11 12:26:31 -0400

    Simple script to use to setup a base vagrant box prior to running the install script for puppet

---
+++ vagrant-install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo apt-get install -y git openssh-server emacs
+sudo rm -fR /etc/puppet
+cd /etc
+sudo git clone https://github.com/PIH/mirebalais-puppet.git puppet
+cd puppet
+sudo git checkout malawi
