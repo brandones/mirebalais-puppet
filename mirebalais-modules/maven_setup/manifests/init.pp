63ac132d3825ba5d2d2761b831497591bef14c3c
mogoodrich <mgoodrich@pih.org>
2015-11-13 12:02:16 -0500

    first pass at creating module to install and configure maven

--- mirebalais-modules/maven_setup/manifests/init.pp
+++ mirebalais-modules/maven_setup/manifests/init.pp
@@ -2,14 +2,12 @@
 # Install Maven
 class maven_setup() {
 
-  class {'maven':
-      version => "3.2.5",
+  package { 'maven':
+    ensure => installed,
   }
 
   # defaults for all maven{} declarations
   Maven {
-    user  => "maven", # you can make puppet run Maven as a specific user instead of root, useful to share Maven settings and local repository
-    group => "maven", # you can make puppet run Maven as a specific group
     repos => "http://mavenrepo.openmrs.org/nexus/content/repositories/public2"
   }
 
