0e0dc147fc66e7c1509f1685989b53b19626ccb4
mogoodrich <mgoodrich@pih.org>
2015-11-13 12:20:10 -0500

    fix name of bahmni_openmrs class

--- mirebalais-modules/bahmni_openmrs/manifests/init.pp
+++ mirebalais-modules/bahmni_openmrs/manifests/init.pp
@@ -1,4 +1,4 @@
-class bahnmi_openmrs (
+class bahmni_openmrs (
   $openmrs_db = hiera('openmrs_db'),
   $openmrs_db_user = decrypt(hiera('openmrs_db_user')),
   $openmrs_db_password = decrypt(hiera('openmrs_db_password')),
