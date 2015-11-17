50fc62b26138e94e51b1b3855fdb513a1b58591b
mogoodrich <mgoodrich@pih.org>
2015-11-17 17:48:16 -0500

    bahmni: revised bahmniapps deploy--fix directory for bahmniapps

--- mirebalais-modules/bahmni/manifests/app.pp
+++ mirebalais-modules/bahmni/manifests/app.pp
@@ -2,12 +2,18 @@ class bahmni::app (
   $tomcat = hiera('tomcat')
 ){
 
+  file { "/usr/local/${tomcat}/webapps/bahmniapps":
+    ensure  => directory,
+    require => [Service[$tomcat]] ,
+  }
+
   # TODO make this something that relies on a true deployment process, and allows use to apply updates
   wget::fetch { 'download-bahmniapps':
     source      => 'http://bamboo.pih-emr.org/bahmniapps-repo/bahmniapps.zip',
     destination => "/usr/local/${tomcat}/webapps/bahmniapps/bahmniapps.zip",
     timeout     => 0,
-    verbose     => false
+    verbose     => false,
+    require => File["/usr/local/${tomcat}/webapps/bahmniapps"]
   }
 
   exec { 'unzip_bahmniapps':
