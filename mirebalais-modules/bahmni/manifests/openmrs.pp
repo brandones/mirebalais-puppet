62e5e3b4168675d1e807e5571a90673a1c073820
mogoodrich <mgoodrich@pih.org>
2015-11-18 12:32:44 -0500

    bahmni: fix typo, add comments

--- mirebalais-modules/bahmni/manifests/openmrs.pp
+++ mirebalais-modules/bahmni/manifests/openmrs.pp
@@ -149,7 +149,7 @@ class bahmni::openmrs (
 
   # TODO this is just temporary, until we get a workable version of the bahmnicore module
   wget::fetch { 'download-bahmnicore':
-    source      => 'http://bamboo.pih-emr.org/bahmni-repo/bahmnicore-omod-0.78-SNAPSHOT.omdd',
+    source      => 'http://bamboo.pih-emr.org/bahmni-repo/bahmnicore-omod-0.78-SNAPSHOT.omod',
     destination => "/home/${tomcat}/.OpenMRS/modules/bahmnicore-0.78-SNAPSHOT.omod",
     timeout     => 0,
     verbose     => false,
