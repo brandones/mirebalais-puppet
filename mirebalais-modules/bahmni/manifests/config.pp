4eee10247e8104c9333ebdea7f54b22b7eac6326
mogoodrich <mgoodrich@pih.org>
2015-11-18 12:11:42 -0500

    bahmni: switch to deploy wellbody config

--- mirebalais-modules/bahmni/manifests/config.pp
+++ mirebalais-modules/bahmni/manifests/config.pp
@@ -30,7 +30,7 @@ class bahmni::config (
   vcsrepo { "/usr/local/${tomcat}/webapps/bahmni-config":
     ensure   => latest,
     provider => git,
-    source   => 'https://github.com/Bhamni/default-config.git',     # TODO switch to use implementation-specific config
+    source   => 'https://github.com/PIH/wellbody-config.git',
     revision => 'master',                                           # TODO switch to be driven by variable (which comes from manifest?)
     require => [Service[$tomcat],Package['git']]
   }
