d45eedd6c8b1e1ba42446ab97c5683ef151ec739
mogoodrich <mgoodrich@pih.org>
2016-01-22 17:25:35 -0500

    create default node for site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,24 @@
 Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
 
 node default {
+
+  class { 'apt':
+    always_apt_update => true,
+  }
+
+  include mailx
+  include ntpdate
+  include apt_upgrades
+  include wget
+
+  include java
+  include mysql_setup
+  include apache2
+  include tomcat
+
+  include openmrs
+  include openmrs::initial_setup
+
 }
 
 node 'emr.hum.ht' {
