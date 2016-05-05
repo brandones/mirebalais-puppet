b5e04e1a8897aee77305900112ef77c2b7cf08e3
mogoodrich <mgoodrich@pih.org>
2016-05-05 10:39:20 -0400

    add check to make sure ntp is installed

--- mirebalais-modules/ntpdate/manifests/init.pp
+++ mirebalais-modules/ntpdate/manifests/init.pp
@@ -5,6 +5,11 @@ class ntpdate(
   $server_3 = hiera('ntp_server_3'),
   $server_4 = hiera('ntp_server_4')
 ) {
+
+  package { 'ntp':
+    ensure => installed
+  }
+
   file { '/etc/ntp.conf':
     content => template('ntpdate/ntp.conf.erb')
   }
@@ -20,19 +25,22 @@ class ntpdate(
 
   exec { 'stop ntp':
     command     => 'service ntp stop',
-    subscribe   => [ File['/etc/ntp.conf'], File['/etc/timezone'] ],
-    refreshonly => true
+    subscribe   => [ File['/etc/ntp.conf'], File['/etc/timezone'], File['/etc/defaullt/rcS'] ],
+    refreshonly => true,
+    require => Package['ntp']
   }
 
   exec { 'update time':
     command     => 'ntpdate-debian',
     subscribe   => Exec['stop ntp'],
-    refreshonly => true
+    refreshonly => true,
+    require => Package['ntp']
   }
 
   exec { 'start ntp':
     command     => 'service ntp start',
     subscribe   => Exec['update time'],
-    refreshonly => true
+    refreshonly => true,
+    require => Package['ntp']
   }
 }
