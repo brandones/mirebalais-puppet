a57cdcb3ad5db15f71e833284590be46294690cb
mogoodrich <mgoodrich@pih.org>
2016-05-17 17:14:30 -0400

    refactor ubuntu node

--- mirebalais-modules/ubuntu/manifests/init.pp
+++ mirebalais-modules/ubuntu/manifests/init.pp
@@ -1,25 +1,13 @@
 class ubuntu (
-  $restrict_root_login = hiera('restrict_root_login')
+  $permit_root_login = hiera('permit_root_login')
 ){
 
-  notice("runniing with restrict root login ${restrict_root_login}")
-
-  package { "ruby-augeas" :
-    ensure => installed
+  file { "/etc/ssh/sshd_config":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    content => template("ubuntu/ssh_config.erb")
   }
-
-
-
-    notice("restricting root login")
-
-    # we change default root login to only allow key-based access
-    augeas { "sshd_config":
-      context => "/etc/ssh/sshd_config",
-      changes => [
-        "set PermitRootLogin without-password"
-      ],
-      require => Package["ruby-augeas"]
-    }
 
 
 }
