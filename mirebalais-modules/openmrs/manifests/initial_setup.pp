43df3bce215957009e002ab7d24ea54fb80f8cea
mogoodrich <mgoodrich@pih.org>
2016-06-09 17:47:53 -0400

    add back in missing tomcat variable

--- mirebalais-modules/openmrs/manifests/initial_setup.pp
+++ mirebalais-modules/openmrs/manifests/initial_setup.pp
@@ -2,6 +2,7 @@ class openmrs::initial_setup(
   $openmrs_db = hiera('openmrs_db'),
   $openmrs_db_user = decrypt(hiera('openmrs_db_user')),
   $openmrs_db_password = decrypt(hiera('openmrs_db_password')),
+  $tomcat = hiera('tomcat')
 ) {
 
   mysql_database { $openmrs_db :
