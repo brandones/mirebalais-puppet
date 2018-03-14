e363ef8b73e363b3761128a44371fbe42372c0f9
James Mbabazi <jmbabazi@pih.org>
2018-03-14 15:42:08 -0400

    Edited ntp.conf.erb \           installl.sh \               openmrs init.pp \               mysql init.pp \
    Added a new file with the path for puppet apply

--- mirebalais-modules/mysql_setup/manifests/init.pp
+++ mirebalais-modules/mysql_setup/manifests/init.pp
@@ -29,12 +29,6 @@ class mysql_setup (
     notify  => Service['mysqld']
   }
 
-  file { '/root/.my.cnf':
-    ensure  => absent,
-    require => Package['mysql-server-5.6'],
-    notify  => Service['mysqld']
-  }
-
   # make sure the mysql 5,5 is uninstalled, as well as the custom "mysql" package we put in place
   # note that we put the proper my.cnf in place first because any restarting of mysql requires this package to be present
   package { 'mysql-client-5.5':
