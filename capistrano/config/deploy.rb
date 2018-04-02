23636b3557fce821d1efe344c79fbf852643388a
mgoodrich <mark.goodrich@live.com>
2018-04-02 14:34:22 -0400

    break out gem install into separate script

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -2,8 +2,7 @@ namespace :deploy do
   desc 'Deploys the app with puppet'
   task :default do
     run("cd /etc/puppet && git pull")
-    run("cd /etc/puppet && bundle")
-    run("cd /etc/puppet && librarian-puppet install")
-    run("cd /etc/puppet && puppet apply -v manifests/site.pp")
+    run("cd /etc/puppet && ./gem-update.sh")
+    run("cd /etc/puppet && ./puppet-apply.sh site")
   end
 end
