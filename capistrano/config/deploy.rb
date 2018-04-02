d0581e09b42ffaa9e86220d0d5b1af8d2c328847
mgoodrich <mark.goodrich@live.com>
2018-04-02 15:00:36 -0400

    break out gem install into separate script; set to use specific version of rake in 14.04 context

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -2,7 +2,7 @@ namespace :deploy do
   desc 'Deploys the app with puppet'
   task :default do
     run("cd /etc/puppet && git pull")
-    run("cd /etc/puppet && ./gem-update.sh")
-    run("cd /etc/puppet && ./puppet-apply.sh site")
+    run("cd /etc/puppet && chmod 0750 gem-update.sh && ./gem-update.sh")
+    run("cd /etc/puppet && chmod 0750 puppet-apply.sh && ./puppet-apply.sh site")
   end
 end
