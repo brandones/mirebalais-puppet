120c4a98fb3770ca13c3eb1852533254a82cd9a8
mgoodrich <mark.goodrich@live.com>
2018-04-19 11:42:10 -0400

    stop doing gem updates automatically as part of capistrano deploy

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -2,7 +2,6 @@ namespace :deploy do
   desc 'Deploys the app with puppet'
   task :default do
     run("cd /etc/puppet && git pull")
-    run("cd /etc/puppet && ./gem-update.sh")
     run("cd /etc/puppet && ./puppet-apply.sh site")
   end
 end
